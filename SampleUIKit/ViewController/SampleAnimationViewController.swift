cf5ef15f6257185d2acb2a30bb820a8809021ce7
fuwamaki <variety.fuwa.3@gmail.com>
2023-08-25 14:41:25 +0900

    add blockLabel on SampleAnimation

--- SampleUIKit/ViewController/SampleAnimationViewController.swift
+++ SampleUIKit/ViewController/SampleAnimationViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 final class SampleAnimationViewController: UIViewController {
 
+    @IBOutlet private weak var blockLabel: UILabel! {
+        didSet {
+            blockLabel.alpha = 0.0
+        }
+    }
+
     static func instantiate() -> SampleAnimationViewController {
         let storyboard = UIStoryboard(name: String(describing: self), bundle: Bundle(for: self))
         return storyboard.instantiateInitialViewController() as! SampleAnimationViewController
@@ -16,5 +22,13 @@ final class SampleAnimationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: [.curveEaseIn], animations: {
+            self.blockLabel.alpha = 1.0
+        }, completion: nil)
     }
 }
